e8a0a1fc3a3c471bbc9baa128c279846af0da38b
Max Greenblatt <max.greenblatt@gmail.com>
2025-02-05 01:19:51 -0500

    Update entry.sh

--- entry.sh
+++ entry.sh
@@ -5,14 +5,29 @@ set -e
 
 [[ "${DEBUG}" == "true" ]] && set -x
 
+# Print the initial value of FTP_PASSWORD_HASH (should be empty)
+echo "Initial FTP_PASSWORD_HASH: $FTP_PASSWORD_HASH"
+
+# Set the FTP_PASSWORD_HASH environment variable from the secret file
+export FTP_PASSWORD_HASH=$(cat /run/secrets/amcrest_ftp_pwd_hash_20250204)
+
+# Print the value of FTP_PASSWORD_HASH after setting it
+echo "1. Updated FTP_PASSWORD_HASH: $FTP_PASSWORD_HASH"
+
 # Generate password if hash not set
 if [[ ! -z "${FTP_PASSWORD}" ]] && [[ -z "${FTP_PASSWORD_HASH}" ]]; then
   FTP_PASSWORD_HASH="$(echo "${FTP_PASSWORD}" | mkpasswd -s -m sha-512)"
 fi
 
+# Print the value of FTP_PASSWORD_HASH after setting it
+echo "2. Updated FTP_PASSWORD_HASH: $FTP_PASSWORD_HASH"
+
 if [[ ! -z "${FTP_USER}" ]] || [[ ! -z "${FTP_PASSWORD_HASH}" ]]; then
   /add-virtual-user.sh -d "${FTP_USER}" "${FTP_PASSWORD_HASH}"
 fi
+
+# Print the value of FTP_PASSWORD_HASH after setting it
+echo "3. Updated FTP_PASSWORD_HASH: $FTP_PASSWORD_HASH"
 
 # Support multiple users
 while read -r user; do
